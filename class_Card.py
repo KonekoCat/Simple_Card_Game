d9e0e32afbae827835c3f83666f9cddb1701e2a6
Koneko <georgesu604012007jp@gmail.com>
2022-04-04 16:00:12 +0900

    Title: Card Class Complete
    Implement the Setter & Getter (Property)
    add method: .look()

--- class_Card.py
+++ class_Card.py
@@ -7,9 +7,35 @@ Created on Mon Apr  4 14:30:49 2022
 
 class Card():
     __SUIT = dict(enumerate(['♣', '♦', '♥', '♠']))
-    __VALUE = ['A'] + [i for i in range(2, 11)] + list("JQK")
+    __VALUE = ['A'] + [str(i) for i in range(2, 11)] + list("JQK")
     
     def __init__(self, _suit: int, _value: int):
         self.__suit = _suit
-        self.__value = _value
+        self.__value = _value - 1
+    
+    def look(self) -> str:
+        s = Card.__SUIT[self.__suit] + Card.__VALUE[self.__value]
+        return s
+
+
+    @property
+    def suit(self) -> str:
+        return Card.__SUIT[self.__suit]
+    @suit.setter
+    def suit(self, _suit):
+        self.__suit = _suit
+    
+    @property
+    def value(self) -> str:
+        return Card.__VALUE[self.__value]
+    @value.setter
+    def value(self, _value):
+        self.__value = _value - 1
+
+
+# confirm class
+if __name__ == "__main__":
+    card = Card(0, 8)
+    card.suit = 2
+    print(card.look())
 
