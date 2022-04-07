3e7fb1534444aeffca03b1d6d4b4772dacd95c3b
Koneko <georgesu604012007jp@gmail.com>
2022-04-07 22:04:07 +0900

    Minute Adjustment

--- Field.py
+++ Field.py
@@ -4,16 +4,28 @@ Created on Thu Apr  7 19:00:44 2022
 
 @author: George Su
 """
+import copy
 
 import Card
 import Deck
 import Player
 
 class Field():
-    def __init__(self):
+    def __init__(self, _p_list: list, _deck: Deck):
+        self.player_list = [i for i in _p_list.copy()]
+        self.deck = copy.deepcopy(_deck)
+    
+    def update_player(self, player: Player, player_num: int):
+        pass
+    
+    def update_deck(self, deck: Deck):
+        pass
+    
+    def update_card(self, player_num: int):
         pass
 
-
+    def show(self):
+        pass
 
 
 
