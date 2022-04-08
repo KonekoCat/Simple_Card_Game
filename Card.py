a2a0716db63369ae55b64bbaf97ec20c3e2d2725
Koneko <georgesu604012007jp@gmail.com>
2022-04-08 15:51:05 +0900

    Minute Adjustment
    Add constructor's value check and raise the error if use wrong value

--- Card.py
+++ Card.py
@@ -15,6 +15,7 @@ class Card():
     __SUIT = dict(enumerate(['♣', '♦', '♥', '♠']))
     __VALUE = ['A'] + [str(i) for i in range(2, 11)] + list("JQK")
     
+    # Constructor
     def __init__(self, _suit: int, _value: int):
         if not (0 <= _suit <= 3):
             raise ValueError("Card suit not in 0 ~ 3.")
@@ -40,7 +41,7 @@ class Card():
 
 # confirm class
 # check all methods
-if __name__ == "__main__":
+def main():
     card = Card(0, 8)
     print(card.suit)
     print(card.value)
@@ -49,4 +50,9 @@ if __name__ == "__main__":
     c_list = list()
     c_list.append(Card(0, 8))
     print(c_list[0].look())
+
+if __name__ == "__main__":
+    main()
+
+
 
